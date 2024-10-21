dd531e53ee1c573468675eb25383dbe5f8a6fa29
ajrepos <antonioalguacil94@gmail.com>
2024-10-21 13:16:12 +0200

    Actualizacion

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<?php
-//Archivo Adrian Montes Bastida
-   echo "Hello World";
-   echo "hola";
-   echo "Adrian Montes Bastida";
-
-   //Archivo Antonio Jesus Alguacil Espejo
+//Archivo Antonio Jesus Alguacil Espejo
    echo "Hello World";
    echo "hola";
    echo "Antonio Jesus Alguacil Espejo";
